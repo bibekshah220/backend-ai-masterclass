282b94627b5b310e4854a8950a3a1bb9e422a7f5
bibekshah220 <bibekshah425@gmail.com>
2026-07-08 22:45:56 +0545

    feat: implement basic Express server with a root route and environment variable configuration

--- server.js
+++ server.js
@@ -3,3 +3,15 @@ import express from "express";
 import dotenv from "dotenv";
 
 dotenv.config();
+
+
+const port = process.env.PORT || 8080;
+
+const app = express();  
+
+app.get("/", (req, res) => {
+  return res.status(200).json({ message: "Hello from Docker" });
+});
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
